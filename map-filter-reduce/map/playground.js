3865f5f7441856758c798a618f868e2a48ff919c
brenoxdd4 <brenothegamer14@hotmail.com>
2022-05-06 22:52:17 -0300

    map, filter e reduce.

---
+++ map-filter-reduce/map/playground.js
@@ -0,0 +1,36 @@
+function mapSemThis(arr) {
+  arr.map(function (item) {
+    return item * 2;
+  });
+}
+
+const num = [2, 4, 6, 8, 10];
+
+console.log(mapSemThis(nums));
+
+console.log(nums);
+
+
+/* 
+
+const maçã = {
+  value: 2,
+}
+
+const laranja = {
+  value: e,
+}
+
+function mapComthis(arr, thisArg) {
+  return arr.map(function(item){
+  item * this.value;
+  }, thisArg);
+};
+
+const nums = [1, 2];
+
+console.log(mapComThis(nums, maçã);
+
+console.log('this -> maçã', mapComthis(nums, maçã));
+
+console.log('this -> laranja', mapComthis(nums, maçã));
